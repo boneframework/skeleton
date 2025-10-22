c82697a901e336cdb1f448955ef7f4fadfe88c88
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-10-22 20:23:35 +0200

    add bone natve config

---
+++ config/bone-native.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'bone-native' => [
+        'deepLink' => 'exp://192.168.0.204:8081/--/',
+        'verifier' => 'RaYFJonXO4pxNh7w5ygj90D8ruMPvwigU.BPMNp.O9~-oym396zbL0Lf9wGPPJSWkg1Lrz.4rHgbn_XwoyapfcTfXIONjhlOcA78y11PQIt1ueFlCbJ6DZ892G3HpLkt' /** @see https://tonyxu-io.github.io/pkce-generator/ */
+    ],
+];
+
