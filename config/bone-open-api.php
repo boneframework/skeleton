edcc08b9d5c2b9401abc57e6e0964e8d8155e95c
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-11-04 20:30:28 +0100

    update env

--- config/bone-open-api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'docs' => 'data/docs/openapi.yaml',
-    'swaggerClient' => [
-        'clientId' => 'Your swagger client id',
-        'clientSecret' => 'Your swagger client secret',
-    ],
-];
--- config/bone-paseto.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'bone-paseto' => [
-        'sharedKey' => 'tH1rtYtw0Ch4r4ct3rPasswordBlahXx',
-    ],
-];
